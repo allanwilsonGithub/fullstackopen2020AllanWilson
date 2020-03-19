793398c23b8f6c4228857d5ebc17395d6f81fbc1
Allan Wilson <allan@allanwilson.net>
2020-03-20 01:28:02 +0200

    .

--- part2/countries/src/components/displayNames.js
+++ part2/countries/src/components/displayNames.js
@@ -1,18 +1,33 @@
 import React from 'react'
 
-TODO get this working:
-const DisplayNames = ({ countries, newFilterString }) => countries.map((entry, i) => {
-        if (1 === 1) {
-          return (
-            <p>Too many matches. Refine your filter</p>
-          )}})
+const DisplayNames = ( {countries, newFilterString} ) => {
+  const filteredNames = 0
+  return ListNames({ countries, newFilterString, filteredNames })
+}
 
-const ListNames = ({ countries, newFilterString }) => countries.map((entry, i) => {
-        if (entry.name.toLowerCase().includes(newFilterString.toLowerCase()))
-          return (
-            <p key={i}>{entry.name}</p>
-          )
-        }
+const ListNames = ({ countries, newFilterString, filteredNames }) => countries.map((entry, i) => {
+  if (entry.name.toLowerCase().includes(newFilterString.toLowerCase()))
+    filteredNames = filteredNames+1
+    console.log('FilteredNames: ', filteredNames)
+    console.log('newFilterString', newFilterString)
+    return (
+      <p key={i}>{entry.name}</p>
+    )
+  }
 )
 
 export default DisplayNames
+
+
+
+
+/*
+function DisplayNames1 ({countries, newFilterString}){
+  if (countries.length > 10) {
+    return (
+      <p>Too many matches. Refine your filter</p>
+    )} else {
+      return ListNames (countries={countries}, newFilterString={newFilterString})
+    }
+}
+*/

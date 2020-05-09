41c4c316f47712076ae740c4a48dbfe880509e29
Allan Wilson <allan@allanwilson.net>
2020-05-10 00:40:21 +0300

    .

--- part2/countries/src/components/displayNames.js
+++ part2/countries/src/components/displayNames.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-
 const DisplayNames = ({ countries,newFilterString }) => countries.map((entry, i) => {
   if (entry.name.toLowerCase().includes(newFilterString.toLowerCase())){
   return (
       <p key={i}>{entry.name}</p>
          )
+} else {
+  return ""
 }})
 
 export default DisplayNames
